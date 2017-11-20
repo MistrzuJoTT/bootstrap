839e6c56946a6b5dce802d1689e0ec50f7a6c428
MistrzuJoTT <jamana113@gmail.com>
2017-11-20 11:40:32 +0100

    add page

---
+++ assets/js/vendors/main.js
@@ -0,0 +1,42 @@
+
+// SCROOL TO
+$(document).ready(function(){
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    } 
+  });
+});
+
+
+// FADE IN CARS
+$(function(){  // $(document).ready shorthand
+  $('#cars-area').fadeIn('slow');
+});
+
+$(document).ready(function() {
+    
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+    
+        /* Check the location of each desired element */
+        $('.hideme').each( function(i){
+            
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'1'},1400);                    
+            }            
+        });    
+    });   
+});
+
